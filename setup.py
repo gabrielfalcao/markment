4deae680163a22f36faa85c0b072e2b952826197
Gabriel Falcao <gabriel@nacaolivre.org>
2013-05-15 15:02:58 -0400

    updating version in missing files

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ requirements = [
 ]
 
 setup(name='markment',
-      version='0.1.4',
+      version='0.1.5',
       description=(u'Structured documentation generator for markdown'),
       author=u'Gabriel Falcao',
       author_email='gabriel@nacaolivre.org',
