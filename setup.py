6ca4ba18cdbecb3752cb61b31139b31a0a51d307
Gabriel Falcao <gabriel@nacaolivre.org>
2013-05-23 16:35:58 -0400

    New release: 0.2.12

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ requirements = [
 ]
 
 setup(name='markment',
-      version='0.2.11',
+      version='0.2.12',
       description=(u'Structured documentation generator for markdown'),
       author=u'Gabriel Falcao',
       author_email='gabriel@nacaolivre.org',
