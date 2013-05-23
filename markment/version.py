57fa83e3437e66be719346c35c0795af58e4c713
Gabriel Falcao <gabriel@nacaolivre.org>
2013-05-23 15:27:42 -0400

    New release: 0.2.8

--- markment/version.py
+++ markment/version.py
@@ -18,4 +18,4 @@
 
 from __future__ import unicode_literals
 
-version = __version__ = '0.2.7'
+version = __version__ = '0.2.8'
--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ requirements = [
 ]
 
 setup(name='markment',
-      version='0.2.7',
+      version='0.2.8',
       description=(u'Structured documentation generator for markdown'),
       author=u'Gabriel Falcao',
       author_email='gabriel@nacaolivre.org',
