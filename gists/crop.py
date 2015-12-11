e8dcd9e7766d2eca4fed09daf1d5e800e539f9ce
Nik Nyby <nnyby@columbia.edu>
2015-12-11 16:28:38 -0500

    Use Pillow instead of PIL
    As generally recommended now... fixes this test failure:
    https://travis-ci.org/gabrielfalcao/markment/jobs/73996866

--- gists/crop.py
+++ gists/crop.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import sys
-from PIL import Image
+from Pillow import Image
 from markment.fs import Node
 
 screenshots = Node('spec/screenshots').grep('[.]png$')
